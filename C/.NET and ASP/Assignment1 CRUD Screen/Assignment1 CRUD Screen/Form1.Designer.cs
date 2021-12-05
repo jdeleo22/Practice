c7ab3376b13e65ccc8f1a8c26c0aeb1c1e460183
JohnD <29313225+jdeleo22@users.noreply.github.com>
2021-12-05 18:46:52 -0500

    add crud practice

---
+++ C/.NET and ASP/Assignment1 CRUD Screen/Assignment1 CRUD Screen/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Assignment1_CRUD_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProductDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductStandardPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductFinish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.lblSearchDesc = new System.Windows.Forms.Label();
+            this.txtSearchDesc = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblAvgDisplay = new System.Windows.Forms.Label();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductDescription,
+            this.ProductStandardPrice,
+            this.ProductFinish});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(427, 545);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ProductDescription
+            // 
+            this.ProductDescription.DataPropertyName = "ProductDescription";
+            this.ProductDescription.HeaderText = "ProductDescription";
+            this.ProductDescription.Name = "ProductDescription";
+            this.ProductDescription.Width = 120;
+            // 
+            // ProductStandardPrice
+            // 
+            this.ProductStandardPrice.DataPropertyName = "ProductStandardPrice";
+            this.ProductStandardPrice.HeaderText = "ProductStandardPrice";
+            this.ProductStandardPrice.Name = "ProductStandardPrice";
+            this.ProductStandardPrice.Width = 150;
+            // 
+            // ProductFinish
+            // 
+            this.ProductFinish.DataPropertyName = "ProductFinish";
+            this.ProductFinish.HeaderText = "ProductFinish";
+            this.ProductFinish.Name = "ProductFinish";
+            this.ProductFinish.Width = 120;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Location = new System.Drawing.Point(12, 12);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(95, 31);
+            this.btnProducts.TabIndex = 1;
+            this.btnProducts.Text = "Show Products";
+            this.btnProducts.UseVisualStyleBackColor = true;
+            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+            // 
+            // lblSearchDesc
+            // 
+            this.lblSearchDesc.AutoSize = true;
+            this.lblSearchDesc.Location = new System.Drawing.Point(12, 73);
+            this.lblSearchDesc.Name = "lblSearchDesc";
+            this.lblSearchDesc.Size = new System.Drawing.Size(148, 13);
+            this.lblSearchDesc.TabIndex = 2;
+            this.lblSearchDesc.Text = "Search Product Descriptions: ";
+            // 
+            // txtSearchDesc
+            // 
+            this.txtSearchDesc.Location = new System.Drawing.Point(166, 70);
+            this.txtSearchDesc.Name = "txtSearchDesc";
+            this.txtSearchDesc.Size = new System.Drawing.Size(100, 20);
+            this.txtSearchDesc.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(353, 12);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 31);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.AutoSize = true;
+            this.lblAvg.Location = new System.Drawing.Point(12, 105);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblAvg.Size = new System.Drawing.Size(134, 13);
+            this.lblAvg.TabIndex = 5;
+            this.lblAvg.Text = "Average Price of Products:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(288, 68);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(116, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblAvgDisplay
+            // 
+            this.lblAvgDisplay.AutoSize = true;
+            this.lblAvgDisplay.Location = new System.Drawing.Point(166, 105);
+            this.lblAvgDisplay.Name = "lblAvgDisplay";
+            this.lblAvgDisplay.Size = new System.Drawing.Size(0, 13);
+            this.lblAvgDisplay.TabIndex = 7;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(288, 100);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(116, 23);
+            this.btnAverage.TabIndex = 8;
+            this.btnAverage.Text = "Show Average Price";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(197, 29);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(0, 13);
+            this.lblTest.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 694);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lblAvgDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblAvg);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtSearchDesc);
+            this.Controls.Add(this.lblSearchDesc);
+            this.Controls.Add(this.btnProducts);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Label lblSearchDesc;
+        private System.Windows.Forms.TextBox txtSearchDesc;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductStandardPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductFinish;
+        private System.Windows.Forms.Label lblAvgDisplay;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
---
+++ C/.NET and ASP/Assignment1 CRUD Screen/Assignment1 CRUD Screen/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace Assignment1_CRUD_Screen
+{
+    public partial class Form1 : Form
+    {
+
+        
+       
+
+     SqlCommand mycmd;
+        DataTable mydt;
+        SqlConnection myconn;
+        SqlDataAdapter myadapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            
+            myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\johnd\\Documents\\.Net framework\\PVFC\\PVFC.mdf;Integrated Security=True;Connect Timeout=30";   //name of db, type of db, userid, pw, security lvls, timeouts
+            myconn.Open();
+
+
+            mycmd = new SqlCommand();
+            mycmd.CommandType = CommandType.StoredProcedure;
+            mycmd.CommandText = "SelectAllProducts";
+
+            mycmd.Connection = myconn;
+
+            myadapter = new SqlDataAdapter();
+            myadapter.SelectCommand = mycmd;
+
+            mydt = new DataTable();
+            myadapter.Fill(mydt);
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = mydt;
+
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            mycmd = new SqlCommand();
+            mycmd.Connection = myconn;
+            //desc finish price
+
+            //update
+            mycmd.CommandText = "Update Product_T set ProductDescription = @ProductDesc, ProductFinish = @ProductFinish, ProductStandardPrice = @ProductPrice where ProductID=@id";
+
+            mycmd.Parameters.Add("@ProductDesc", SqlDbType.VarChar, 50, "ProductDescription");
+            mycmd.Parameters.Add("@ProductFinish", SqlDbType.VarChar, 50, "ProductFinish");
+            mycmd.Parameters.Add("@ProductPrice", SqlDbType.VarChar, 50, "ProductStandardPrice");
+            mycmd.Parameters.Add("@id", SqlDbType.Int, 50, "ProductID");
+            myadapter.UpdateCommand = mycmd;
+
+            //delete
+            mycmd = new SqlCommand();
+            mycmd.Connection = myconn;
+            mycmd.CommandText = "Delete Product_T where ProductID=@id ";
+            mycmd.Parameters.Add("@id", SqlDbType.Int, 50, "ProductID");
+            myadapter.DeleteCommand = mycmd;
+
+            //insert
+            mycmd = new SqlCommand();
+            mycmd.Connection = myconn;
+            mycmd.CommandText = "Insert INTO Product_T (ProductDescription, ProductFinish, ProductStandardPrice) Values (@ProductDesc, @ProductFinish, @ProductPrice)";
+
+            mycmd.Parameters.Add("@ProductDesc", SqlDbType.VarChar, 50, "ProductDescription");
+            mycmd.Parameters.Add("@ProductFinish", SqlDbType.VarChar, 50, "ProductFinish");
+            mycmd.Parameters.Add("@ProductPrice", SqlDbType.VarChar, 50, "ProductStandardPrice");
+            myadapter.InsertCommand = mycmd;
+
+
+            try
+            {
+                myadapter.Update(mydt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Updated record is outdated, reload then try again" + ex);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\johnd\\Documents\\.Net framework\\PVFC\\PVFC.mdf;Integrated Security=True;Connect Timeout=30";   //name of db, type of db, userid, pw, security lvls, timeouts
+            myconn.Open();
+
+
+            mycmd = new SqlCommand();
+            mycmd.CommandType = CommandType.Text;
+             //mycmd.CommandType = CommandType.StoredProcedure;
+            //mycmd.CommandText = "SearchProductDesc";
+            
+            //lblTest.Text = 
+            mycmd.CommandText = "Select ProductDescription, ProductStandardPrice, ProductFinish from Product_T where ProductDescription like @description";
+            mycmd.Parameters.Add("@description", SqlDbType.VarChar);
+            mycmd.Parameters["@description"].Value = "%" + txtSearchDesc.Text + "%";
+            //wildcard %%
+            //mycmd.Parameters["@description"].Value = "%" + textBox1.Text + "%";
+
+            mycmd.Connection = myconn;
+    
+            myadapter = new SqlDataAdapter();
+            myadapter.SelectCommand = mycmd;
+
+            mydt = new DataTable();
+            myadapter.Fill(mydt);
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = mydt;
+        }
+
+        
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+
+            //display average
+            int sum = 0;
+            int average = 0;
+            int rows = dataGridView1.Rows.Count;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+
+                sum += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value);
+
+            }
+            
+            average = sum / rows;
+
+            lblAvgDisplay.Text = average.ToString();
+        }
+    }
+}
